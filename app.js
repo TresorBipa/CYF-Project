ed6cd0db8ac99a7f6b3e3928384f3dfb16a711be
Tresor83 <66829445+Tresor83@users.noreply.github.com>
2022-04-23 14:54:12 +0200

    CYF App

---
+++ app.js
@@ -0,0 +1,20 @@
+function getNumberOrString(value) {
+    // Convert a string value to a number if possible
+    let number_value = Number(value);
+    if (Number.isNaN(number_value)) {
+      return value
+    } else {
+      return number_value
+    }
+  }
+  
+  
+  
+  document.getElementById('button').addEventListener('click', (event) => {
+    let element_list = document.getElementById('list');
+    let new_li = document.createElement('li');
+    new_li.innerText = getNumberOrString(document.getElementById('text').value);
+  
+    element_list.appendChild(new_li);
+  
+  });
